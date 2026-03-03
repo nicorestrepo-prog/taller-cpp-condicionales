3510f4bd8412f76cb9e3030d2870d9fe55bc5798
nicorestrepo-prog <nicolasrestrepo669@gmail.com>
2026-03-03 08:44:27 -0500

    Create ejercicio_01.cpp

---
+++ ejercicio_01/ejercicio_01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double celsius;
+    cout << "Ingrese temperatura en Celsius: ";
+    cin >> celsius;
+
+    double fahrenheit = (celsius * 9.0/5.0) + 32;
+    double kelvin = celsius + 273.15;
+    double rankine = (celsius + 273.15) * 9.0/5.0;
+
+    cout << fixed << setprecision(2);
+    cout << "Fahrenheit: " << fahrenheit << endl;
+    cout << "Kelvin: " << kelvin << endl;
+    cout << "Rankine: " << rankine << endl;
+
+    return 0;
+}
