e606e9b7093567b292183262752b885bd94414f8
nicorestrepo-prog <nicolasrestrepo669@gmail.com>
2026-03-03 09:15:32 -0500

    Create ejercicio_10.cpp

---
+++ ejercicio_01/ejercicio_02/ejercicio_03/ejercicio_04/ejercicio_05/ejercicio_06/ejercicio_07/ejercicio_08/ejercicio_09/ejercicio_10/ejercicio_10.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    char tipo, cupon;
+    double monto;
+    bool finSemana;
+
+    cout << "Tipo cliente (V/N): ";
+    cin >> tipo;
+    cout << "Monto compra: ";
+    cin >> monto;
+    cout << "Codigo cupon (A/B/C): ";
+    cin >> cupon;
+    cout << "Es fin de semana? (1=Si 0=No): ";
+    cin >> finSemana;
+
+    double descuento = 0;
+
+    if (tipo=='V') descuento += 0.20;
+    if (monto>200) descuento += 0.10;
+    if (finSemana) descuento += 0.05;
+
+    switch(cupon) {
+        case 'A': descuento += 0.05; break;
+        case 'B': descuento += 0.10; break;
+        case 'C': descuento += 0.15; break;
+    }
+
+    descuento = min(descuento, 0.50);
+
+    double precioFinal = monto*(1-descuento);
+
+    cout << "Precio final: " << precioFinal;
+
+    return 0;
+}
+
+feat: resuelto ejercicio 10 - mini motor de reglas de negocio
+}
