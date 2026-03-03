cc5cd27e22b9144d6345fa7e4d77a89897789cff
nicorestrepo-prog <nicolasrestrepo669@gmail.com>
2026-03-03 08:57:58 -0500

    Create ejercicio_03.cpp

---
+++ ejercicio_01/ejercicio_02/ejercicio_03/ejercicio_03.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int LEER = 0b001;
+    const int ESCRIBIR = 0b010;
+    const int EJECUTAR = 0b100;
+
+    int permisos;
+    cout << "Ingrese valor de permisos (0-7): ";
+    cin >> permisos;
+
+    if (permisos & LEER)
+        cout << "Permiso de LEER\n";
+    if (permisos & ESCRIBIR)
+        cout << "Permiso de ESCRIBIR\n";
+    if (permisos & EJECUTAR)
+        cout << "Permiso de EJECUTAR\n";
+
+    return 0;
+}
