b86d3098f7984af26c7794225c8ccb596af89b6d
nicorestrepo-prog <nicolasrestrepo669@gmail.com>
2026-03-03 09:09:18 -0500

    Create ejercicio_08.cpp

---
+++ ejercicio_01/ejercicio_02/ejercicio_03/ejercicio_04/ejercicio_05/ejercicio_06/ejercicio_07/ejercicio_08/ejercicio_08.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double saldo = 1000;
+    int opcion;
+    int transacciones = 0;
+
+    cout << "1.Depositar 2.Retirar 3.Consultar: ";
+    cin >> opcion;
+
+    switch(opcion) {
+        case 1: {
+            double monto;
+            cin >> monto;
+            saldo += monto;
+            transacciones++;
+            break;
+        }
+        case 2: {
+            double monto;
+            cin >> monto;
+            if (monto <= saldo) {
+                saldo -= monto;
+                transacciones++;
+            } else
+                cout << "Fondos insuficientes\n";
+            break;
+        }
+        case 3:
+            cout << "Saldo: " << saldo << endl;
+            break;
+    }
+
+    cout << "Transacciones: " << transacciones;
+    return 0;
+}
+}
