fa9f43f49016df4e4e056a9369a42ea782fc5a29
nicorestrepo-prog <nicolasrestrepo669@gmail.com>
2026-03-03 09:12:28 -0500

    Create ejercicio_09.cpp

---
+++ ejercicio_01/ejercicio_02/ejercicio_03/ejercicio_04/ejercicio_05/ejercicio_06/ejercicio_07/ejercicio_08/ejercicio_09/ejercicio_09.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double peso, altura;
+    cout << "Ingrese peso (kg): ";
+    cin >> peso;
+    cout << "Ingrese altura (m): ";
+    cin >> altura;
+
+    double imc = peso/(altura*altura);
+
+    if (imc < 18.5)
+        cout << "Bajo peso";
+    else if (imc < 25)
+        cout << "Normal";
+    else if (imc < 30)
+        cout << "Sobrepeso";
+    else if (imc < 35)
+        cout << "Obesidad I";
+    else
+        cout << "Obesidad II";
+
+    return 0;
+}
