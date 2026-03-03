18d290f7e9fe6c142c40e8dc5d6a3e7da761b5c9
nicorestrepo-prog <nicolasrestrepo669@gmail.com>
2026-03-03 08:53:23 -0500

    Create ejercicio_02.cpp

---
+++ ejercicio_01/ejercicio_02/ejercicio_02.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cout << "Ingrese tres lados: ";
+    cin >> a >> b >> c;
+
+    if (a+b>c && a+c>b && b+c>a) {
+
+        if (a==b && b==c)
+            cout << "Equilatero\n";
+        else if (a==b || a==c || b==c)
+            cout << "Isosceles\n";
+        else
+            cout << "Escaleno\n";
+
+        double mayor = max(a, max(b,c));
+        double sumaCuadrados = pow(a,2)+pow(b,2)+pow(c,2)-pow(mayor,2);
+
+        if (pow(mayor,2)==sumaCuadrados)
+            cout << "Rectangulo\n";
+        else if (pow(mayor,2)<sumaCuadrados)
+            cout << "Acutangulo\n";
+        else
+            cout << "Obtusangulo\n";
+    }
+    else
+        cout << "No es un triangulo valido\n";
+
+    return 0;
+}
