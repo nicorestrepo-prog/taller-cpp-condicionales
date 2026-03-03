9fcb5a4e742160dc834ebb661250c233071599d2
nicorestrepo-prog <nicolasrestrepo669@gmail.com>
2026-03-03 09:06:58 -0500

    Create ejercicio_06.cpp

---
+++ ejercicio_01/ejercicio_02/ejercicio_03/ejercicio_04/ejercicio_05/ejercicio_06/ejercicio_06.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=5, b=3;
+
+    cout << "Original: a=" << a << " b=" << b << endl;
+
+    int temp=a;
+    a=b;
+    b=temp;
+    cout << "Temporal: a=" << a << " b=" << b << endl;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout << "Aritmetico: a=" << a << " b=" << b << endl;
+
+    a^=b;
+    b^=a;
+    a^=b;
+    cout << "XOR: a=" << a << " b=" << b << endl;
+
+    return 0;
+}
